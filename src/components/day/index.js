498eba8a5106fb7837d6806b0c62727369654c4a
Euan Goddard <euan.goddard@gmail.com>
2017-11-22 19:35:18 +0000

    Added logic to determine when door can be opened

--- src/components/day/index.js
+++ src/components/day/index.js
@@ -1,37 +1,48 @@
 import { h, Component } from 'preact';
 import style from './style';
-import { isFutureDate } from '../../utils/date';
+import { isFutureDate, formatDateDelta } from '../../utils/date';
+import { OPEN_STATE } from '../../utils/opens-state';
+
 
 export class Day extends Component {
 
   constructor() {
     super();
-    // set initial time:
     this.state = {
       isFuture: true,
+      deltaFormatted: null,
+      isOpened: false,
     };
   }
   componentDidMount() {
-    // update time every second
+    this.setState({ isOpened: OPEN_STATE.opens[this.props.day] });
     this.timer = setInterval(() => {
-      const { day } = this.props;
-      const date = new Date(2017, 10, day + 1);
-      this.setState({ isFuture: isFutureDate(date) });
+      const isFuture = isFutureDate(this.date);
+      if (this.state.isFuture !== isFuture) {
+        this.setState({ isFuture });
+      }
+      if (isFuture) {
+        const delta = this.date - new Date();
+        const deltaFormatted = formatDateDelta(delta);
+        if (this.state.deltaFormatted !== deltaFormatted) {
+          this.setState({ deltaFormatted });
+        }
+      }
+
     }, 1000);
   }
 
   componentWillUnmount() {
-    // stop when not renderable
     clearInterval(this.timer);
   }
 
-  render({ day, offsetX }, { isFuture }) {
+  render({ day, offsetX }, state) {
     const mod2Class = day % 2 === 0 ? style.cover : style.box;
     const dayClass = style[`day-${day + 1}`];
     return <div class={`${style.day} ${mod2Class} ${dayClass}`}>
       <h1 class={style.label}>{day + 1}</h1>
-      <p>{isFuture.toString()}</p>
       <div class={style.background} style={this.getStyle(day, offsetX)}></div>
+      {this.renderGaurd(state)}
     </div>;
   }
 
@@ -54,8 +65,25 @@ export class Day extends Component {
     return { transform };
   }
 
-  isFuture(currentTime) {
-    const currentTimeDate = new Date(currentTime);
+  renderGaurd({ isFuture, deltaFormatted, isOpened }) {
+    const content = isFuture ? <p>Open in {deltaFormatted}</p> : <button type="button" onClick={this.open}>Open</button>;
+    if (!isOpened) {
+      return <div class={style.guard}>
+        <h1>{this.props.day + 1} Dec</h1>
+        {content}
+      </div>;
+    }
 
+  }
+
+  open = e => {
+    OPEN_STATE.openDay(this.props.day);
+    this.setState({ isOpened: true });
+  }
+
+  get date() {
+    const { day } = this.props;
+    const date = new Date(2017, 10, day + 1);
+    return date;
   }
 }
---
+++ src/utils/opens-state.js
@@ -0,0 +1,45 @@
+class OpensState {
+
+  static KEY = 'opens';
+
+  constructor() {
+    this.opens = this.loadState();
+  }
+
+  openDay(day) {
+    this.opens[day] = true;
+    this.saveState();
+  }
+
+  loadState() {
+    let stateRaw
+    try {
+      const stateRaw = localStorage.getItem(OpensState.KEY);
+    } catch (e) { }
+
+    let state;
+    if (stateRaw) {
+      try {
+        state = JSON.parse(stateRaw);
+      } catch (e) { }
+    }
+
+    if (!state) {
+      state = {};
+      for (let i = 0; i < 25; i++) {
+        state[i] = false;
+      }
+    }
+    return state;
+  }
+
+  saveState() {
+    const stateRaw = JSON.stringify(this.opens);
+    try {
+      localStorage.setItem(OpensState.KEY, stateRaw);
+    } catch (e) { }
+
+  }
+}
+
+export const OPEN_STATE = new OpensState();
