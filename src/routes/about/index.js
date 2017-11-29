bae418151ae16536eaf161e8d6c045097d405ce6
Euan Goddard <euan.goddard@gmail.com>
2017-11-29 20:40:13 +0000

    Tidied up about component

--- src/routes/about/index.js
+++ src/routes/about/index.js
@@ -5,12 +5,11 @@ import style from './style';
 
 export default class About extends Component {
   render() {
-    console.log(style);
     return <div class={style.about}>
       <h1>About this card</h1>
       <p>
         This card was created by <a href="https://github.com/euangoddard" target="_blank">Euan Goddard</a> to celebrate Christmas 2017 and to
-        learn <a href="https://preactjs.com/" target="_blank">Preact</a>. It is available as an installed progressive web app (i.e. works offline) &ndash; this was achieved
+        learn <a href="https://preactjs.com/" target="_blank">Preact</a>. It is available as an installable progressive web app (i.e. works offline) &ndash; this was achieved
         using <a href="https://github.com/developit/preact-cli" target="_blank">Preact CLI</a>. The source code is available <a href="https://github.com/euangoddard/xmas-17" target="_blank">on github</a>.
       </p>
       <p>All images were obtained from the marvellous <a href="https://unsplash.com/search/photos/christmas" target="_blank">Unsplash</a> collection of free images.</p>
