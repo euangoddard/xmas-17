7b4e5d84e3b00987d4c51006b09f7f510fe9e712
Euan Goddard <euan.goddard@gmail.com>
2017-11-29 20:40:02 +0000

    Added support for google analytics

--- src/components/app.js
+++ src/components/app.js
@@ -4,13 +4,28 @@ import { Router } from 'preact-router';
 import Home from '../routes/home';
 import About from '../routes/about';
 
+const TRACKING_ID = 'UA-110442895-1';
+
 export default class App extends Component {
+
+  componentDidMount() {
+    if (typeof window !== 'undefined"' && typeof document !== 'undefined') {
+      const script = document.createElement('script');
+      script.async = true;
+      script.src = `https://www.googletagmanager.com/gtag/js?id=${TRACKING_ID}`;
+      document.head.appendChild(script);
+      this.gtag('js', new Date());
+      this.gtag('config', TRACKING_ID, { send_page_view: false });
+    }
+  }
+
   /** Gets fired when the route changes.
    *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
    *	@param {string} event.url	The newly routed URL
    */
   handleRoute = e => {
     this.currentUrl = e.url;
+    this.gtag('config', TRACKING_ID, { page_path: e.url });
   };
 
   render() {
@@ -22,5 +37,12 @@ export default class App extends Component {
         </Router>
       </div>
     );
+  }
+
+  gtag() {
+    if (typeof window !== "undefined") {
+      window.dataLayer = window.dataLayer || [];
+      window.dataLayer.push(arguments);
+    }
   }
 }
